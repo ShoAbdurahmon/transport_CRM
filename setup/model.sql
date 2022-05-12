77a64dec5b6987ee3707d67967516cd078d1b5ee
Abdurahmon <shohusniddinovabdurahmon@gmail.com>
2022-05-12 22:46:55 +0500

    create files

--- setup/model.sql
+++ setup/model.sql
@@ -13,12 +13,11 @@ create database transport_sale;
 
 -- extensions
 
-create extension if not exists "uuid-ossp";
 
 drop table if exists branch;
 
 create table branch (
-    branch_id uuid default uuid_generate_v4() primary key,
+    branch_id serial not null primary key,
     branch_name varchar(255) not null,
     branch_address varchar(255) not null,
     time_created timestamptz default current_timestamp
@@ -28,8 +27,8 @@ create table branch (
 drop table if exists transport;
 
 create table transport(
-    transport_id uuid default uuid_generate_v4() primary key,
-    branch_id uuid not null references branches(branch_id),
+    transport_id serial not null primary key,
+    branch_id int not null references branch(branch_id),
     transport_name character varying(255) not null,
     transport_model character varying(255) not null,
     transport_color character varying(255) not null,
@@ -41,10 +40,36 @@ create table transport(
 drop table if exists stuff;
 
 create table stuff(
-    stuff_id uuid default uuid_generate_v4() primary key,
-    branch_id uuid not null references branches(branch_id),
+    stuff_id serial not null primary key,
+    branch_id int not null references branch(branch_id),
     username character varying(255) not null,
     password character varying(255) not null,
+    birth_date varchar(16) not null,
     gender character varying(255) not null,
     time_created timestamptz default current_timestamp
 );
+
+
+drop table if exists transport_permission;
+
+create table transport_permission(
+    transport_permission_id serial not null primary key,
+    transport_create boolean default false,
+    transport_read boolean default false,
+    transport_delete boolean default false,
+    transport_update boolean default false,
+    branch_id serial not null references branch(branch_id)
+    staff_id serial not null references stuff(stuff_id)
+);
+
+drop table if exists branch_permission;
+
+create table branch_permission(
+    branch_permission_id serial not null primary key,
+    branch_create boolean default false,
+    branch_read boolean default false,
+    branch_delete boolean default false,
+    branch_update boolean default false,
+    branch_id serial not null references branch(branch_id),
+    stuff_id serial not null references stuff(stuff_id)
+)
